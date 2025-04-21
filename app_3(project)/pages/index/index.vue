1a25635fa5264c4e81dfac34bb3815641980c88b
Louioskarmen <ayxc000@qq.com>
2025-04-21 22:33:44 +0800

    5

---
+++ app_3(project)/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class='container'>
+		<view class='layout'>
+			<view class='box' v-for='item in 5'>
+				<img src="../../static/logo.png" alt="hhh">
+				<view class='text'>@@@@</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
