323b9664b38d7a4895f2861514ff09e6674870d1
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-06-07 23:19:59 +0800

    1

---
+++ KK通讯录/pages/list/list.vue
@@ -0,0 +1,18 @@
+<template>
+	<view>
+		<unicloud-db v-slot:default="{data, loading, error, options}" collection="kk1">
+			<view v-if="error">{{error.message}}</view>
+			<view v-else>
+				{{data}}
+			</view>
+		</unicloud-db>
+	</view>
+</template>
+
+<script setup lang='ts'>
+	
+</script>
+
+<style scoped lang='scss'>
+	       
+</style>
